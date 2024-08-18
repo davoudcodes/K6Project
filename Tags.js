0bddc16fad828038f55d586e019112fb803a301e
Davoud Yousuf <davoudyousuf@gmail.com>
2024-08-18 13:03:17 +0530

    Added Basic tags based on inbuilt property

---
+++ Tags.js
@@ -0,0 +1,17 @@
+import http from "k6/http";
+
+
+export const options =
+{
+    thresholds: {
+        http_req_duration:['p(95)<=1000'],
+        'http_req_duration{status:200}': ['p(95)<=1000']  //tag with inbuilt property
+    }
+}
+
+export default function()
+{
+    http.get('https://run.mocky.io/v3/f5ae742d-bd2c-4548-a811-1ffa974bab35');   //generate 200 and 201 on mocky.io
+    http.get('https://run.mocky.io/v3/e87c4f63-027f-4aaa-a635-0a744318d776?mocky-delay=1500ms'); //simulated delay to fail req duration check
+
+}
