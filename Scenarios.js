2659f98b3e3efc2fdd4bfc94cb085f838e69f0c8
Davoud Yousuf <davoudyousuf@gmail.com>
2024-08-18 10:02:55 +0530

    Added assertion for the status of response frm page

--- Scenarios.js
+++ Scenarios.js
@@ -4,8 +4,11 @@ import { check } from 'k6';
 export default function()
 {
     const res = http.get('https://test.k6.io');
-    check(true, {
+   /* check(true, {
         'true is true': (value) => value === true
-    });
+    });*/
+    check(res, {
+        'Response is 200': (r)=> r.status === 200
+    })
 
 }
